e792429e5da289b1881ff34430c247d417e81579
green <green@cganalytics.org>
2015-09-14 15:33:58 -0700

    etl exercise

--- etl/src/main/java/Etl.java
+++ etl/src/main/java/Etl.java
@@ -1,8 +1,13 @@
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Etl {
    public Map<String, Integer> transform(Map<Integer, List<String>> old) {
-      return null;
+       HashMap<String, Integer> new_map = new HashMap<String, Integer>();
+       for (Map.Entry<Integer, List<String>> entry : old.entrySet()) {
+           for (String l : entry.getValue()) {
+               new_map.put(l.toLowerCase(), entry.getKey());
+           }
+       }
+       return new_map;
    }
 }
